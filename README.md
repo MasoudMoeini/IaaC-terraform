82db61f9f32257cc7911f923c3d3595b059bd342
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-07-04 23:09:17 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Iaac terraform
-Deployment of Nginx web server to Docker Desktop with terraform<br/>
-[Reference](https://learn.hashicorp.com/tutorials/terraform/install-cli?in=terraform/aws-get-started)
+# Deploying Infrastructur into Google-Cloud 
 
 ```
 terraform init
--- main.tf
+++ main.tf
@@ -1,24 +1,11 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
+provider "google" {
+  credentials = file("warm-rookery-325409-9706537f6ad9.json")
+
+  project = "warm-rookery-325409"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
